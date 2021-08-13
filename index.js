f47e164784086b7cdc643903b0438519a307d4c6
pranik chainani <pranikc@gmail.com>
2021-08-13 10:49:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    if (record.find(each => each["result"] === "W")) {
+        const getRecord = record.find(each => each["result"] === "W")
+        return getRecord["year"]
+    }
+    else {
+        return undefined
+    }
+}
